3b837740224f686d30d117ebd65e2da1498cf91b
Claus Due <claus@namelesscoder.net>
2014-09-30 02:25:42 +0200

    [DOC] Add license header to files

--- tests/unit/NamelessCoder/GizzleGitPlugins/GizzlePlugins/AbstractGitPluginTest.php
+++ tests/unit/NamelessCoder/GizzleGitPlugins/GizzlePlugins/AbstractGitPluginTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * This file belongs to the namelesscoder/gizzle-git-plugins package
+ *
+ * Copyright (c) 2014, Claus Due
+ *
+ * Released under the MIT license, of which the full text
+ * was distributed with this package in file LICENSE.txt
+ */
+
 namespace NamelessCoder\GizzleGitPlugins\Tests\Unit\GizzlePlugins;
 
 use NamelessCoder\Gizzle\Payload;
--- tests/unit/NamelessCoder/GizzleGitPlugins/Resolver/GitCommandResolverTest.php
+++ tests/unit/NamelessCoder/GizzleGitPlugins/Resolver/GitCommandResolverTest.php
@@ -1,5 +1,16 @@
 <?php
+
+/**
+ * This file belongs to the namelesscoder/gizzle-git-plugins package
+ *
+ * Copyright (c) 2014, Claus Due
+ *
+ * Released under the MIT license, of which the full text
+ * was distributed with this package in file LICENSE.txt
+ */
+
 namespace NamelessCoder\GizzleGitPlugins\Tests\Unit\Resolver;
+
 use NamelessCoder\GizzleGitPlugins\Resolver\GitCommandResolver;
 
 /**
