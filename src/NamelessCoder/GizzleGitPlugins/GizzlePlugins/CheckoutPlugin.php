3cb3a77ff52c52d22c93188c10bfc98bd9db73ee
Claus Due <claus@namelesscoder.net>
2014-10-11 12:56:19 +0200

    [TASK] Initialise Checkout plugin

--- src/NamelessCoder/GizzleGitPlugins/GizzlePlugins/CheckoutPlugin.php
+++ src/NamelessCoder/GizzleGitPlugins/GizzlePlugins/CheckoutPlugin.php
@@ -19,4 +19,66 @@ use NamelessCoder\Gizzle\PluginInterface;
  */
 class CheckoutPlugin extends AbstractGitPlugin implements PluginInterface {
 
+	const COMMAND = 'checkout';
+	const COMMAND_FORCE = '--force';
+	const COMMAND_TRACK = '--track';
+	const COMMAND_NOTRACK = '--no-track';
+	const COMMAND_DETACH = '--detach';
+	const COMMAND_ORPHAN = '--orphan';
+	const COMMAND_NEWBRANCH = '-b';
+	const OPTION_NEWBRANCH = 'newbranch';
+	const OPTION_STARTPOINT = 'startpoint';
+	const OPTION_UPSTREAM = 'upstream';
+	const OPTION_FORCE = 'force';
+	const OPTION_TRACK = 'track';
+	const OPTION_NOTRACK = 'notrack';
+	const OPTION_DETACH = 'detach';
+	const OPTION_ORPHAN = 'orphan';
+
+	/**
+	 * @param Payload $payload
+	 */
+	public function process(Payload $payload) {
+		$directory = $this->getDirectorySettingOrFail();
+		$directory = sprintf($directory, $payload->getRepository()->getName());
+		$git = $this->resolveGitCommand();
+		$branch = $this->getSettingValue(self::OPTION_BRANCH, $payload->getRepository()->getMasterBranch());
+		$track = $this->getSettingValue(self::OPTION_TRACK, FALSE);
+		$noTrack = $this->getSettingValue(self::OPTION_NOTRACK, FALSE);
+		$start = $this->getSettingValue(self::OPTION_STARTPOINT);
+		$force = $this->getSettingValue(self::OPTION_FORCE, FALSE);
+		$detach = $this->getSettingValue(self::OPTION_DETACH, FALSE);
+		$orphan = $this->getSettingValue(self::OPTION_ORPHAN, FALSE);
+		$newBranch = $this->getSettingValue(self::OPTION_NEWBRANCH, FALSE);
+		$command = array($git, self::COMMAND);
+		if (TRUE === $noTrack) {
+			$command[] = self::COMMAND_NOTRACK;
+		} elseif (TRUE === $track) {
+			$trackBranch = $this->getSettingValue(self::OPTION_UPSTREAM, $payload->getRepository()->getMasterBranch());
+			$command[] = self::COMMAND_TRACK;
+			$command[] = escapeshellarg($trackBranch);
+		}
+		if (TRUE === $force) {
+			$command[] = self::COMMAND_FORCE;
+		}
+		if (TRUE === $orphan) {
+			$command[] = self::COMMAND_ORPHAN;
+		}
+		if (TRUE === $detach) {
+			$command[] = self::COMMAND_DETACH;
+		}
+		if (TRUE === $newBranch) {
+			$command[] = self::COMMAND_NEWBRANCH;
+		}
+		$command[] = escapeshellarg($branch);
+		if (NULL !== $start) {
+			$command[] = escapeshellarg($start);
+		}
+		$output = array(
+			'Executing Git checkout command: ' . implode(' ', $command)
+		);
+		$output = array_merge($output, $this->executeGitCommand($command));
+		$payload->getResponse()->addOutputFromPlugin($this, $output);
+	}
+
 }
--- tests/unit/NamelessCoder/GizzleGitPlugins/GizzlePlugins/CheckoutPluginTest.php
+++ tests/unit/NamelessCoder/GizzleGitPlugins/GizzlePlugins/CheckoutPluginTest.php
@@ -10,6 +10,7 @@
  */
 
 namespace NamelessCoder\GizzleGitPlugins\Tests\Unit\GizzlePlugins;
+use NamelessCoder\GizzleGitPlugins\GizzlePlugins\CheckoutPlugin;
 
 /**
  * Class CheckoutPluginTest
@@ -17,10 +18,154 @@ namespace NamelessCoder\GizzleGitPlugins\Tests\Unit\GizzlePlugins;
 class CheckoutPluginTest extends \PHPUnit_Framework_TestCase {
 
 	/**
-	 * @test
+	 * @dataProvider getSettingsAndExpectedCommands
+	 * @param array $settings
+	 * @param array $expectedCommand
 	 */
-	public function placeholder() {
-		$this->markTestIncomplete('Not implemented yet');
+	public function testProcess($settings, $expectedCommand) {
+		$mock = $this->getMock('NamelessCoder\\GizzleGitPlugins\\GizzlePlugins\\CheckoutPlugin',
+			array('resolveGitCommand', 'executeGitCommand'));
+		$repository = $this->getMock('NamelessCoder\\Gizzle\\Repository', array('getMasterBranch'));
+		$response = $this->getMock('NamelessCoder\\Gizzle\\Response', array('addOutputFromPlugin'));
+		$response->expects($this->once())->method('addOutputFromPlugin')->with($mock);
+		$repository->expects($this->any())->method('getMasterBranch')->will($this->returnValue('master'));
+		$payload = $this->getMock('NamelessCoder\\Gizzle\\Payload', array('getRepository', 'getResponse'), array(), '', FALSE);
+		$payload->expects($this->any())->method('getRepository')->will($this->returnValue($repository));
+		$payload->expects($this->once())->method('getResponse')->will($this->returnValue($response));
+		$mock->expects($this->once())->method('resolveGitCommand')->will($this->returnValue('git'));
+		$mock->expects($this->once())->method('executeGitCommand')->with($expectedCommand)->will($this->returnValue(array()));
+		$mock->initialize($settings);
+		$mock->process($payload);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getSettingsAndExpectedCommands() {
+		return array(
+			array(
+				array(CheckoutPlugin::OPTION_DIRECTORY => '.', CheckoutPlugin::OPTION_BRANCH => 'master'),
+				array('git', CheckoutPlugin::COMMAND, escapeshellarg('master'))
+			),
+			array(
+				array(CheckoutPlugin::OPTION_DIRECTORY => '.'),
+				array('git', CheckoutPlugin::COMMAND, escapeshellarg('master'))
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_NEWBRANCH => TRUE
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_NEWBRANCH,
+					escapeshellarg('master')
+				)
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_STARTPOINT => 'refname'
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					escapeshellarg('master'),
+					escapeshellarg('refname')
+				)
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_DETACH => TRUE,
+					CheckoutPlugin::OPTION_ORPHAN => TRUE
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_ORPHAN,
+					CheckoutPlugin::COMMAND_DETACH,
+					escapeshellarg('master')
+				)
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_TRACK => TRUE,
+					CheckoutPlugin::OPTION_UPSTREAM => 'upstream-branch'
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_TRACK,
+					escapeshellarg('upstream-branch'),
+					escapeshellarg('master')
+				)
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_TRACK => TRUE
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_TRACK,
+					escapeshellarg('master'),
+					escapeshellarg('master')
+				)
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_NOTRACK => TRUE
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_NOTRACK,
+					escapeshellarg('master')
+				)
+			),
+			// check: track and notrack mutually exclusive with priority to notrack
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_TRACK => TRUE,
+					CheckoutPlugin::OPTION_NOTRACK => TRUE,
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_NOTRACK,
+					escapeshellarg('master')
+				)
+			),
+			array(
+				array(
+					CheckoutPlugin::OPTION_DIRECTORY => '.',
+					CheckoutPlugin::OPTION_BRANCH => 'master',
+					CheckoutPlugin::OPTION_TRACK => TRUE,
+					CheckoutPlugin::OPTION_FORCE => TRUE
+				),
+				array(
+					'git',
+					CheckoutPlugin::COMMAND,
+					CheckoutPlugin::COMMAND_TRACK,
+					escapeshellarg('master'),
+					CheckoutPlugin::COMMAND_FORCE,
+					escapeshellarg('master')
+				)
+			),
+		);
 	}
 
 }
